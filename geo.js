520b1fc197fc0ea790e602ed064b280b07625d3f
Daniel <Daniel@Daniel-PC>
2014-09-08 23:16:09 -0300

    primer prueba de cuentagotas

--- geo.js
+++ geo.js
@@ -1,7 +1,61 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Cinco saltos  			Latitud: -38.845 Longitud: -68.069   	Altura :282     http://anterior.inta.gov.ar/altovalle/cincosaltos/downld02.txt
+ * Contralmirante Guerrico		Latitud: -39.017 Longitud: -67.667	Altura: 242Mts  http://anterior.inta.gov.ar/altovalle/met/downld02.txt
+ * Picún Leufú                          Latitud: -39.536 Longitud: -69.298	Altura: 393Mts  http://anterior.inta.gov.ar/altovalle/picunleufu/downld02.txt
+ * Coronel Belisle			Latitud: -39.197 Longitud: -65.894	Altura: 128Mts  http://anterior.inta.gov.ar/altovalle/belisle/downld02.txt
+ * Luís Beltrán                         Latitud: -39.317 Longitud: -65.75	Altura: 128Mts  http://anterior.inta.gov.ar/altovalle/lube/downld02.txt
+ * Pomona				Latitud: -39.478 Longitud: -65.653	Altura: 127Mts  http://anterior.inta.gov.ar/altovalle/pomona/downld08.txt
+ * Villa Regina                         Latitud: -39.126 Longitud: -67.106	Altura: 158Mts  http://anterior.inta.gov.ar/altovalle/regina/downld02.txt
+ *NA  C. A. Cordero			Latitud: -38.74  Longitud: -68.11	Altura: 297Mts
+ * San Patricio del Chañar		Latitud: -38.57  Longitud: -68.36	Altura: 334Mts  http://anterior.inta.gov.ar/altovalle/sanpatricio/downld02.txt
  */
+var ema= new Array(7);
+for (var i = 0; i < 7; i++) {
+    ema[i] = new Array(5);
+}
+   ema[0][0] ='Cinco Saltos';
+   ema[0][1] =-38.845;
+   ema[0][2] =-68.069;
+   ema[0][3] =282;
+   ema[0][4] ='http://anterior.inta.gov.ar/altovalle/cincosaltos/downld02.txt';
+   
+   ema[1][0] ='Contralmirante Guerrico';
+   ema[1][1] =-39.017;
+   ema[1][2] =-67.667;
+   ema[1][3] =242;
+   ema[1][4] ='http://anterior.inta.gov.ar/altovalle/met/downld02.txt';
+   
+   ema[2][0] ='Picún Leufú';
+   ema[2][1] =-39.536;
+   ema[2][2] =-69.298;
+   ema[2][3] =393;
+   ema[2][4] ='http://anterior.inta.gov.ar/altovalle/picunleufu/downld02.txt';
+
+   ema[3][0] ='Coronel Belisle';
+   ema[3][1] =-39.197;
+   ema[3][2] =-65.894;
+   ema[3][3] =128;
+   ema[3][4] ='http://anterior.inta.gov.ar/altovalle/belisle/downld02.txt';
+   
+   ema[4][0] ='Pomona';
+   ema[4][1] =-39.478;
+   ema[4][2] =-65.653;
+   ema[4][3] =127;
+   ema[4][4] ='http://anterior.inta.gov.ar/altovalle/pomona/downld08.txt';
+   
+   ema[5][0] ='Villa Regina';
+   ema[5][1] =-39.126;
+   ema[5][2] =-67.106;
+   ema[5][3] =158;
+   ema[5][4] ='http://anterior.inta.gov.ar/altovalle/regina/downld02.txt';   
+ 
+   ema[6][0] ='San Patricio del Chañar';
+   ema[6][1] =-38.57;
+   ema[6][2] =-68.36;
+   ema[6][3] =334;
+   ema[6][4] ='http://anterior.inta.gov.ar/altovalle/sanpatricio/downld02.txt';
+  
+
 function distance(lat1,lon1,lat2,lon2) {
 	var R = 6371; // km (change this constant to get miles)
 	var dLat = (lat2-lat1) * Math.PI / 180;
@@ -15,4 +69,16 @@ function distance(lat1,lon1,lat2,lon2) {
 //	else if (d<=1) return Math.round(d*1000)+"m";
 	return d;
 }
+  function onSuccessGPS (){
+                    var distancia=distance(ema[1][1],ema[1][2],position.coords.latitude,position.coords.longitude)
+                    alert('Latitude: ' + position.coords.latitude + '\n' +
+                    'Longitude: ' + position.coords.longitude + '\n'+
+                    'Distanncia: '+distancia+' km \n') ;
+            }
+            // onError Callback receives a PositionError object
+            //
+    function onErrorGPS(error) {
+                        alert('code: ' + error.code + '\n' +
+                        'message: ' + error.message + '\n');
+                    }
 
