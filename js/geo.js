225c4c7e9d86b788575c830ea6cfff443bf1b2ea
Daniel <Daniel@Daniel-PC>
2014-09-12 14:45:19 -0300

    primer prueba de cuentagotas

--- js/geo.js
+++ js/geo.js
@@ -73,7 +73,8 @@ var menorDistancia= new Array(7);
 function ordenGPS(){
        // calculo la distancia de c/u de las EMA y lo guardo en MenorDistancia
        for (var i = 0; i < 7; i++) {
-                ema[i][5] = distance(ema[i][1],ema[i][2],position.coords.latitude,position.coords.longitude);
+                ema[i][5] = distance(ema[i][1],ema[i][2],-38.57,-68.36);
+                //ema[i][5] = distance(ema[i][1],ema[i][2],position.coords.latitude,position.coords.longitude);
        }
         ema = ema.sort(function(a,b) {
                             return a[5] < b[5];
